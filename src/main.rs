8a8c01142c483bdda0082dd07153866f7e99b23a
Maid Dog <maiddogsrl@gmail.com>
2018-10-04 05:03:19 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use ggez::{Context, GameResult};
 use ggez::conf;
 use ggez::event::{self, EventHandler, Keycode, Mod, MouseButton};
-use ggez::graphics::{self, Color, DrawMode, Point2, Rect};
+use ggez::graphics::{self, Color, DrawMode, DrawParam, Mesh, Point2, Rect};
+use rayon::prelude::*;
 
 /// The target window size width/height wise.
 const WINDOW_SIZE: f32 = 800.0;
 /// The rank of the board, or how many squares make up a side.
-const RANK: usize = 4;
+const RANK: usize = 100;
 /// The size of a single square on the board.
 const SPACE_SIZE: f32 = WINDOW_SIZE / RANK as f32;
 /// The radius of a player's piece.
@@ -17,8 +18,32 @@ use self::mainstate::{MainState, Piece};
 
 impl EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        if !self.board.iter().any(Option::is_none) {
+        if !self.board.par_iter().any(Option::is_none) {
+            if self.is_auto() {
+                self.reset();
+            }
+
             self.check_winner();
+        } else if self.is_auto() {
+            let mut rng = rand::thread_rng();
+
+            let candidates = (0..RANK*RANK).into_par_iter().filter(|index| {
+                self.valid_space(index % RANK, index / RANK)
+            }).collect::<Vec<usize>>();
+
+            let choice = rand::sample(&mut rng, candidates, 1);
+
+            if choice.len() > 0 {
+                let index = choice[0];
+                self.place(index % RANK, index / RANK);
+            } else {
+                self.next_turn();
+
+                //  If neither side has a valid move, then end the game
+                if !self.has_move() {
+                    self.reset();
+                }
+            }
         } else {
             //  If no valid move can be made, they forfeit their turn
             if !self.has_move() {
@@ -34,65 +59,79 @@ impl EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let start = std::time::Instant::now();
+
         graphics::clear(ctx);
         
         let mut color_flag = false;
         //  Keep track of which index has the spot with the most captures
         let mut best_spot = None;
 
+        for col in 0..RANK {
+            for row in 0..RANK {
+                if col == 0 {
+                    color_flag = !color_flag;
+                }
+
+                let rect_color = if self.valid_space(col, row) {
+                    let total = self.captures(col, row).len();
+                    if best_spot.is_none() || total > best_spot.unwrap_or(0) {
+                        best_spot = Some(row * RANK + col);
+                    }
+                    Some(Color::from((255, 19, 22)))
+                } else if color_flag {
+                    Some(Color::from((158, 19, 22)))
+                } else {
+                    None
+                };
+
+                color_flag = !color_flag;
+
+                if rect_color.is_some() {
+                    graphics::draw_ex(ctx, &self.rect,
+                        DrawParam {
+                            dest: Point2::new(col as f32 * SPACE_SIZE, row as f32 * SPACE_SIZE),
+                            color: rect_color,
+                            .. Default::default()
+                        })?;
+                }
+            }
+        }
+        println!("r{:?}", start.elapsed());
+
         for (index, piece) in self.board.iter().enumerate() {
             let col = (index % RANK) as f32;
             let row = (index / RANK) as f32;
 
-            if col == 0.0 {
-                color_flag = !color_flag;
-            }
-
-            if self.valid_space(col as usize, row as usize) {
-                graphics::set_color(ctx, Color::from((255, 19, 22)))?;
-                let total = self.captures(col as usize, row as usize).len();
-                if best_spot.is_none() || total > best_spot.unwrap_or(0) {
-                    best_spot = Some(index);
-                }
-            } else if color_flag {
-                graphics::set_color(ctx, Color::from((158, 19, 22)))?;
-            } else {
-                graphics::set_color(ctx, Color::from((59, 122, 87)))?;
-            }
-
-            color_flag = !color_flag;
-
-            graphics::rectangle(ctx,
-                DrawMode::Fill,
-                Rect::new(col * SPACE_SIZE, row * SPACE_SIZE, SPACE_SIZE, SPACE_SIZE))?;
-
             if let Some(p) = piece {
-                if *p == Piece::Black {
-                    graphics::set_color(ctx, graphics::BLACK)?;
+                let player_color = if *p == Piece::Black {
+                    graphics::BLACK
                 } else {
-                    graphics::set_color(ctx, graphics::WHITE)?;
-                }
+                    graphics::WHITE
+                };
 
-                graphics::circle(ctx, 
-                    DrawMode::Fill, 
-                    Point2::new(col * SPACE_SIZE + SPACE_SIZE / 2.0, row * SPACE_SIZE + SPACE_SIZE / 2.0),
-                    RADIUS,
-                    0.0001)?;
+                graphics::draw_ex(ctx, &self.circle,
+                    DrawParam {
+                        dest: Point2::new(col * SPACE_SIZE + SPACE_SIZE / 2.0, row * SPACE_SIZE + SPACE_SIZE / 2.0),
+                        color: Some(player_color),
+                        .. Default::default()
+                    })?;
+                        
             }
         }
-
-        graphics::set_color(ctx, Color::from((0, 255, 0)))?;
         
         if let Some(best) = best_spot {
-            graphics::rectangle(ctx,
-                DrawMode::Fill,
-                Rect::new((best % RANK) as f32 * SPACE_SIZE,
-                    (best / RANK) as f32 * SPACE_SIZE,
-                    SPACE_SIZE,
-                    SPACE_SIZE))?;
+            graphics::draw_ex(ctx, &self.rect,
+                DrawParam {
+                    dest: Point2::new((best % RANK) as f32 * SPACE_SIZE, (best / RANK) as f32 * SPACE_SIZE),
+                    color: Some(Color::from((0, 255, 0))),
+                    .. Default::default()
+                })?;
         }
 
         graphics::present(ctx);
+
+        println!("{:?}", start.elapsed());
         Ok(())
     }
 
@@ -100,6 +139,14 @@ impl EventHandler for MainState {
         match keycode {
             Keycode::Escape => ctx.quit().unwrap(),
             Keycode::R => self.reset(),
+            Keycode::A => {
+                println!("Auto mode on");
+                self.auto_mode(true);
+            }
+            Keycode::S => {
+                println!("Auto mode off");
+                self.auto_mode(false);
+            }
             _ => {}
         }
     }
@@ -130,7 +177,7 @@ fn main() {
         ctx.filesystem.mount(&path, true);
     }
 
-    let mut game = match MainState::new() {
+    let mut game = match MainState::new(ctx) {
         Ok(game) => game,
         Err(why) => {
             println!("Could not load MainState: {:?}", why);
@@ -138,6 +185,7 @@ fn main() {
         }
     };
 
+    graphics::set_background_color(ctx, Color::from((59, 122, 87)));
     let result = event::run(ctx, &mut game);
 
     if let Err(e) = result {
--- src/mainstate.rs
+++ src/mainstate.rs
@@ -1,4 +1,6 @@
-use ggez::GameResult;
+use ggez::{Context, GameResult};
+use ggez::graphics::{DrawMode, Mesh, Point2};
+use rayon::prelude::*;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Piece {
@@ -17,10 +19,13 @@ pub struct MainState {
     pub board: [Option<Piece>; crate::RANK * crate::RANK],
     turn: Piece,
     winner: Option<Winner>,
+    auto_mode: bool,
+    pub circle: Mesh,
+    pub rect: Mesh,
 }
 
 impl MainState {
-    pub fn new() -> GameResult<Self> {
+    pub fn new(ctx: &mut Context) -> GameResult<Self> {
         let mut board = [None; crate::RANK * crate::RANK];
 
         let idx = crate::RANK / 2;
@@ -30,10 +35,30 @@ impl MainState {
         board[(crate::RANK * idx) + idx - 1] = Some(Piece::Black);
         board[(crate::RANK * idx) + idx] = Some(Piece::White);
 
+        
+        let circle = Mesh::new_circle(ctx,
+            DrawMode::Fill,
+            Point2::new(0.0, 0.0),
+            crate::RADIUS,
+            10.0)?;
+
+        let rect = Mesh::new_polygon(ctx,
+            DrawMode::Fill,
+            &[
+                Point2::new(0.0, 0.0),
+                Point2::new(0.0, crate::SPACE_SIZE),
+                Point2::new(crate::SPACE_SIZE, crate::SPACE_SIZE),
+                Point2::new(crate::SPACE_SIZE, 0.0),
+                Point2::new(0.0, 0.0),
+            ])?;
+
         Ok(Self {
             board,
             turn: Piece::White,
             winner: None,
+            auto_mode: false,
+            circle,
+            rect,
         })
     }
 
@@ -59,10 +84,10 @@ impl MainState {
         }
 
         //  Last spot left is always valid
-        if self.board.into_iter().filter(|x| x.is_none()).count() == 1 {
+        if self.board.into_par_iter().filter(|x| x.is_none()).count() == 1 {
             return true;
         }
-
+        
         //  Check below
         for new_y in y + 1..crate::RANK {
             let new_idx = crate::RANK * new_y + x;
@@ -73,12 +98,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check above
         for new_y in (0..y).rev() {
@@ -90,12 +115,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check left
         for new_x in (0..x).rev() {
@@ -107,12 +132,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check right
         for new_x in x + 1..crate::RANK {
@@ -124,12 +149,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check diagonal down right
         for (new_x, new_y) in (x + 1..crate::RANK).zip(y + 1..crate::RANK) {
@@ -141,12 +166,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check diagonal up right
         for (new_x, new_y) in (x + 1..crate::RANK).zip((0..y).rev()) {
@@ -158,12 +183,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check diagonal down right
         for (new_x, new_y) in (0..x).rev().zip(y + 1..crate::RANK) {
@@ -175,12 +200,12 @@ impl MainState {
                 }
                 break;
             } else if self.board[new_idx].is_none() {
+                flipped = false;
                 break;
             } else {
                 flipped = true;
             }
         }
-        flipped = false;
 
         //  Check diagonal up right
         for (new_x, new_y) in (0..x).rev().zip((0..y).rev()) {
@@ -201,7 +226,7 @@ impl MainState {
         false
     }
 
-    pub fn captures(&self, x: usize, y: usize) -> Vec<usize> {
+    fn check_down(&self, x: usize, y: usize) -> Vec<usize> {
         //  List of positions to flip colors
         let mut captures = Vec::new();
         //  Storing potential flips while searching each direction
@@ -221,7 +246,15 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
+
+        captures
+    }
+
+    fn check_up(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
 
         //  Check above
         for new_y in (0..y).rev() {
@@ -237,7 +270,15 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
+
+        captures
+    }
+
+    fn check_left(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
 
         //  Check left
         for new_x in (0..x).rev() {
@@ -253,7 +294,16 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
+
+        captures
+    }
+    
+
+    fn check_right(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
 
         //  Check right
         for new_x in x + 1..crate::RANK {
@@ -269,7 +319,15 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
+
+        captures
+    }
+    
+    fn check_down_right(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
 
         //  Check diagonal down right
         for (new_x, new_y) in (x + 1..crate::RANK).zip(y + 1..crate::RANK) {
@@ -285,7 +343,15 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
+
+        captures
+    }
+    
+    fn check_up_right(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
 
         //  Check diagonal up right
         for (new_x, new_y) in (x + 1..crate::RANK).zip((0..y).rev()) {
@@ -301,9 +367,17 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
 
-        //  Check diagonal down right
+        captures
+    }
+    
+    fn check_down_left(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
+
+        //  Check diagonal down left
         for (new_x, new_y) in (0..x).rev().zip(y + 1..crate::RANK) {
             let new_idx = crate::RANK * new_y + new_x;
 
@@ -317,9 +391,17 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        candidates.clear();
 
-        //  Check diagonal up right
+        captures
+    }
+    
+    fn check_up_left(&self, x: usize, y: usize) -> Vec<usize> {
+        //  List of positions to flip colors
+        let mut captures = Vec::new();
+        //  Storing potential flips while searching each direction
+        let mut candidates = Vec::new();
+
+        //  Check diagonal up left
         for (new_x, new_y) in (0..x).rev().zip((0..y).rev()) {
             let new_idx = crate::RANK * new_y + new_x;
 
@@ -333,16 +415,28 @@ impl MainState {
                 candidates.push(new_idx);
             }
         }
-        
+
+        captures
+    }
+
+    pub fn captures(&self, x: usize, y: usize) -> Vec<usize> {
+        let mut captures = self.check_down(x, y);
+        captures.append(&mut self.check_up(x, y));
+        captures.append(&mut self.check_left(x, y));
+        captures.append(&mut self.check_right(x, y));
+        captures.append(&mut self.check_down_right(x, y));
+        captures.append(&mut self.check_up_right(x, y));
+        captures.append(&mut self.check_down_left(x, y));
+        captures.append(&mut self.check_up_left(x, y));
         captures
     }
 
     pub fn place(&mut self, x: usize, y: usize) {
         self.board[crate::RANK * y + x] = Some(self.turn);
-
-        for index in self.captures(x, y).iter() {
+        
+        self.captures(x, y).iter().for_each(|index| {
             self.board[*index] = Some(self.turn);
-        }
+        });
 
         self.next_turn();
     }
@@ -368,7 +462,7 @@ impl MainState {
     }
 
     pub fn check_winner(&mut self) -> Winner {
-        let white = self.board.iter().filter(|&x| *x == Some(Piece::White)).count();
+        let white = self.board.par_iter().filter(|&x| *x == Some(Piece::White)).count();
         let black = crate::RANK * crate::RANK - white;
 
         if black > white {
@@ -380,5 +474,13 @@ impl MainState {
         }
 
         self.winner.unwrap()
+    }
+
+    pub fn auto_mode(&mut self, value: bool) {
+        self.auto_mode = value;
+    }
+
+    pub fn is_auto(&self) -> bool {
+        self.auto_mode
     }
 }
